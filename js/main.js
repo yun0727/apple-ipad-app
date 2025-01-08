f92f3428315b3479604bb22cf9f14a37cb10569b
yun <austru00@gmail.com>
2025-01-09 08:21:45 +0900

    Feat: Add , Control Video

--- js/main.js
+++ js/main.js
@@ -85,3 +85,19 @@ const infoEls = document.querySelectorAll('.info')
 infoEls.forEach(function(el){
   io.observe(el)
 })
+
+// 비디오 재생
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.stage .controller--play')
+const pauseBtn = document.querySelector('.stage .controller--pause')
+
+playBtn.addEventListener('click', function(){
+  video.play()
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+})
+pauseBtn.addEventListener('click', function(){
+  video.pause()
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+})
