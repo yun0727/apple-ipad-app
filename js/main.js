9c9748408ceb19bd316eb73d9df8412bf0abfd2e
yun <austru00@gmail.com>
2025-01-08 14:58:31 +0900

    Feat: JS Intersection Observer

--- js/main.js
+++ js/main.js
@@ -71,3 +71,17 @@ function hideSearch(){
   searchDelayEls.reverse()
   searchInputEl.value=''
 }
+
+//요소의 가시성 관찰
+const io = new IntersectionObserver(function(entries){
+  entries.forEach(function(entry){
+    if (!entry.isIntersecting){
+      return
+    }
+    entry.target.classList.add('show')
+  })
+})
+const infoEls = document.querySelectorAll('.info')
+infoEls.forEach(function(el){
+  io.observe(el)
+})
