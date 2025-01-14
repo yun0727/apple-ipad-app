03cf1b68260a82cc6fd488e31e056d119647d407
yun <austru00@gmail.com>
2025-01-14 11:28:22 +0900

    Feat: Navigation Menu Toggle

--- js/main.js
+++ js/main.js
@@ -115,6 +115,30 @@ window.addEventListener('resize', function(){
   }
 })
 
+// 
+const navEl = document.querySelector('nav')
+const navMenuToggleEl = navEl.querySelector('.menu-toggler')
+const navMenuShadowEl = navEl.querySelector('.shadow')
+navMenuToggleEl.addEventListener('click', function(){
+  if (navEl.classList.contains('menuing')){
+    hideNavMenu()
+  } else{
+    showNavMenu()
+  }
+})
+navEl.addEventListener('click', function(event){
+  event.stopPropagation()
+})
+navMenuShadowEl.addEventListener('click', hideNavMenu)
+window.addEventListener('click', hideNavMenu)
+
+function showNavMenu(){
+  navEl.classList.add('menuing')
+}
+function hideNavMenu(){
+  navEl.classList.remove('menuing')
+}
+
 //요소의 가시성 관찰
 const io = new IntersectionObserver(function(entries){
   entries.forEach(function(entry){
